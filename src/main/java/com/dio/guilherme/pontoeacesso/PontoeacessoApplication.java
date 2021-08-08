e100636d4ce15a3879a7b306f163f9b2e0223f11
Guilherme <guilherme0541@gmail.com>
2021-08-08 00:30:13 -0300

    first commit

---
+++ src/main/java/com/dio/guilherme/pontoeacesso/PontoeacessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.guilherme.pontoeacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoeacessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoeacessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/guilherme/pontoeacesso/PontoeacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.guilherme.pontoeacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoeacessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
