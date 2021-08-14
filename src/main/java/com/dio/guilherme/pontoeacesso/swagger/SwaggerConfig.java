38b957645d62853860c4e10890a4e5ad49c7a795
Guilherme Silva <guilherme0541@gmail.com>
2021-08-14 18:34:40 -0300

    Update SwaggerConfig.java

--- src/main/java/com/dio/guilherme/pontoeacesso/swagger/SwaggerConfig.java
+++ src/main/java/com/dio/guilherme/pontoeacesso/swagger/SwaggerConfig.java
@@ -2,42 +2,44 @@ package com.dio.guilherme.pontoeacesso.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
-import springfox.documentation.service.Contact;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-//@EnableSwagger2
-public class SwaggerConfig {
-    
-@Bean
-public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())//basePackage("com.dio.guilherme.pontoeacesso"))
-        .paths(PathSelectors.any())
-        .build()
-        //.apiInfo(apiInfo())
-        ;
-}
-/*
-@Bean
-public ApiInfo apiInfo() {
-    return new ApiInfoBuilder()
-            .title("Controle de Ponto e Acesso REST API")
-            .description("API Rest para controle de ponto e acesso utilizando Spring-boot, H2 e Swagger")
-            .version("1.0.0")
-            .license("Apache License Version 2.0")
-            .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-            .contact(new Contact("Guilherme Silva", "https://github.com/guilherme0541", "guilherme0541@gmail.com"))
-            .build();
-}*/
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
 
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.dio.guilherme.pontoeacesso";
+    private static final String API_TITLE = "Controle de Ponto e Acesso REST API";
+    private static final String API_DESCRIPTION = "API Rest para controle de ponto e acesso utilizando Spring-boot, H2 e Swagger";
+    private static final String CONTACT_NAME = "Guilherme Silva";
+    private static final String CONTACT_GITHUB = "https://github.com/guilherme0541";
+    private static final String CONTACT_EMAIL = "guilherme0541@gmail.com";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version("1.0.0")
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
 }
