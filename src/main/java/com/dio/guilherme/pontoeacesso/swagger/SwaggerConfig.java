00adb713490746ffb7d2246f272f0a6d8e68e7fe
Guilherme Silva <guilherme0541@gmail.com>
2021-08-17 14:02:17 -0300

    Updated for Swagger3

--- src/main/java/com/dio/guilherme/pontoeacesso/swagger/SwaggerConfig.java
+++ src/main/java/com/dio/guilherme/pontoeacesso/swagger/SwaggerConfig.java
@@ -9,12 +9,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.RequestHandlerSelectors.*;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     private static final String BASE_PACKAGE = "com.dio.guilherme.pontoeacesso";
